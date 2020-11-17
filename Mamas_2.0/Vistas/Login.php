912ae564843793147d15a0f2dc060faec856e61a
isra9shadow <isra9movil@hotmail.com>
2020-11-17 12:47:27 +0100

    Cambio nombre ventanas

--- Mamas_2.0/Vistas/Login.php
+++ Mamas_2.0/Vistas/Login.php
@@ -8,79 +8,79 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <!-- Material Design Bootstrap -->
+        <link rel="stylesheet" href="../css/mdb.min.css">
+        <!-- Your custom styles (optional) -->
+        <link rel="stylesheet" href="../css/style.css">
     </head>
     <body>
+
         <?php
         // put your code here
         ?>
-        <!-- Material form login -->
-<div class="card">
+        <header class="row">
+            <div class="col-3 bg-secondary"><p>LOGO</p></div>
 
-  <h5 class="card-header info-color white-text text-center py-4">
-    <strong>Sign in</strong>
-  </h5>
+        </header>
+        <section id="form" class="row">
+            <div class="col-2"></div>
+            <div id="formularioLogin" class="card col-8">
 
-  <!--Card content-->
-  <div class="card-body px-lg-5 pt-0">
+                <h5 class="card-header info-color white-text text-center py-4">
+                    <strong>Inicio de Sesion</strong>
+                </h5>
 
-    <!-- Form -->
-    <form class="text-center" style="color: #757575;" action="#!">
+                <!--Card content-->
+                <div class="card-body px-lg-5 pt-0">
 
-      <!-- Email -->
-      <div class="md-form">
-        <input type="email" id="materialLoginFormEmail" class="form-control">
-        <label for="materialLoginFormEmail">E-mail</label>
-      </div>
+                    <!-- Form -->
+                    <form class="text-center" style="color: #757575;" method="POST" action="../Controlador/Controlador.php">
 
-      <!-- Password -->
-      <div class="md-form">
-        <input type="password" id="materialLoginFormPassword" class="form-control">
-        <label for="materialLoginFormPassword">Password</label>
-      </div>
+                        <!-- Email -->
+                        <div class="md-form">
+                            <input type="email" id="email" class="form-control">
+                            <label for="email">E-mail</label>
+                        </div>
 
-      <div class="d-flex justify-content-around">
-        <div>
-          <!-- Remember me -->
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="materialLoginFormRemember">
-            <label class="form-check-label" for="materialLoginFormRemember">Remember me</label>
-          </div>
-        </div>
-        <div>
-          <!-- Forgot password -->
-          <a href="">Forgot password?</a>
-        </div>
-      </div>
+                        <!-- Password -->
+                        <div class="md-form">
+                            <input type="password" id="password" class="form-control">
+                            <label for="password">Password</label>
+                        </div>
 
-      <!-- Sign in button -->
-      <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">Sign in</button>
+                        <div class="d-flex justify-content-around">
+                            <div>
+                                <!-- Forgot password -->
+                                <a href="../Vistas/olvidado.php">¿Has olvidado tu contraseña?</a>
+                            </div>
+                        </div>
 
-      <!-- Register -->
-      <p>Not a member?
-        <a href="">Register</a>
-      </p>
+                        <!-- Sign in button -->
+                        <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">Sign in</button>
 
-      <!-- Social login -->
-      <p>or sign in with:</p>
-      <a type="button" class="btn-floating btn-fb btn-sm">
-        <i class="fab fa-facebook-f"></i>
-      </a>
-      <a type="button" class="btn-floating btn-tw btn-sm">
-        <i class="fab fa-twitter"></i>
-      </a>
-      <a type="button" class="btn-floating btn-li btn-sm">
-        <i class="fab fa-linkedin-in"></i>
-      </a>
-      <a type="button" class="btn-floating btn-git btn-sm">
-        <i class="fab fa-github"></i>
-      </a>
+                        <!-- Register -->
+                        <p>Not a member?
+                            <a href="../Vistas/registro.php">Registrarse</a>
+                        </p>
 
-    </form>
-    <!-- Form -->
+                    </form>
+                </div>
 
-  </div>
-
-</div>
-<!-- Material form login -->
+            </div>
+            <div class="col-2"></div>
+        </section>
+        <!-- jQuery -->
+        <script type="text/javascript" src="../js/jquery.min.js"></script>
+        <!-- Bootstrap tooltips -->
+        <script type="text/javascript" src="../js/popper.min.js"></script>
+        <!-- Bootstrap core JavaScript -->
+        <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+        <!-- MDB core JavaScript -->
+        <script type="text/javascript" src="../js/mdb.min.js"></script>
+        <!-- Your custom scripts (optional) -->
+        <script type="text/javascript"></script>
     </body>
 </html>
