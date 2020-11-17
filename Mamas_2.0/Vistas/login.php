785e0f7f2c64e63f5f5b07a750eb1dd3b570188f
isra9shadow <isra9movil@hotmail.com>
2020-11-17 18:16:48 +0100

    Correcion en lanzamiento de errores

--- Mamas_2.0/Vistas/login.php
+++ Mamas_2.0/Vistas/login.php
@@ -67,15 +67,17 @@ and open the template in the editor.
                         </div>
                         </section>
                     </div>
-                    <!-- jQuery -->
-                    <script type="text/javascript" src="../js/jquery.min.js"></script>
-                    <!-- Bootstrap tooltips -->
-                    <script type="text/javascript" src="../js/popper.min.js"></script>
-                    <!-- Bootstrap core JavaScript -->
-                    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
-                    <!-- MDB core JavaScript -->
-                    <script type="text/javascript" src="../js/mdb.min.js"></script>
-                    <!-- Your custom scripts (optional) -->
-                    <script type="text/javascript"></script>
-                    </body>
-                    </html>
+                </div>
+        </div>
+        <!-- jQuery -->
+        <script type="text/javascript" src="../js/jquery.min.js"></script>
+        <!-- Bootstrap tooltips -->
+        <script type="text/javascript" src="../js/popper.min.js"></script>
+        <!-- Bootstrap core JavaScript -->
+        <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+        <!-- MDB core JavaScript -->
+        <script type="text/javascript" src="../js/mdb.min.js"></script>
+        <!-- Your custom scripts (optional) -->
+        <script type="text/javascript"></script>
+    </body>
+</html>
