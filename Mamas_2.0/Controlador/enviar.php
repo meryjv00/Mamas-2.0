4ab8f41317303c6370b1adc36a4aec25e9b64a26
isra9shadow <isra9movil@hotmail.com>
2020-11-19 21:33:23 +0100

    Correcion errores
    Validaciones en enviar y login , Captcha y formato.

--- Mamas_2.0/Controlador/enviar.php
+++ Mamas_2.0/Controlador/enviar.php
@@ -16,45 +16,57 @@ and open the template in the editor.
         use PHPMailer\PHPMailer\PHPMailer;
         use PHPMailer\PHPMailer\SMTP;
 
-require_once '../phpmailer/src/Exception.php';
+        require_once '../phpmailer/src/Exception.php';
         require_once '../phpmailer/src/PHPMailer.php';
         require_once '../phpmailer/src/SMTP.php';
         require_once '../Auxiliar/gestionDatos.php';
+        $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+        $recaptcha_secret = '6LdU7-QZAAAAAChZ7pnDbgTL--nSmYG6aJxTMj2f';
+        $recaptcha_response = $_POST['recaptcha_response'];
+        $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+        $recaptcha = json_decode($recaptcha);
 
-        $emailDestino = $_REQUEST['email'];
+        if ($recaptcha->score >= 0.7) {
+            $emailDestino = $_REQUEST['email'];
 
-        $mail = new PHPMailer();
-        try {
-            $mail->isSMTP();
-            $mail->Host = 'smtp.gmail.com';  // Host de conexión SMTP
-            $mail->SMTPAuth = true;
-            $mail->Username = 'AuxiliarDAW2@gmail.com';
-            $mail->Password = 'Chubaca20';
-            $mail->SMTPSecure = 'ssl';
-            $mail->Port = 465;
+            $mail = new PHPMailer();
+            try {
+                $mail->isSMTP();
+                $mail->Host = 'smtp.gmail.com';  // Host de conexión SMTP
+                $mail->SMTPAuth = true;
+                $mail->Username = 'AuxiliarDAW2@gmail.com';
+                $mail->Password = 'Chubaca20';
+                $mail->SMTPSecure = 'ssl';
+                $mail->Port = 465;
 
-            $mail->setFrom('AuxiliarDAW2@gmail.com');
-            $mail->addAddress($emailDestino);
+                $mail->setFrom('AuxiliarDAW2@gmail.com');
+                $mail->addAddress($emailDestino);
 
-            $mail->isHTML(true);
-            $mail->Subject = 'Recupera tu cuenta';
+                $mail->isHTML(true);
+                $mail->Subject = 'Recupera tu cuenta';
 
-            $az = rand(1, 99999);
+                $az = rand(1, 99999);
 
-            $mail->Body = 'Nueva contraseña:<b>' . $az . '</b>';
-            $mail->AltBody = 'Contraseña olvidada';
+                $mail->Body = 'Nueva contraseña:<b>' . $az . '</b>';
+                $mail->AltBody = 'Contraseña olvidada';
 
-            $mail->send();
+                $mail->send();
 
-            if (gestionDatos::setPassword($emailDestino, md5($az))) {
-                $_SESSION['mensaje'] = 'Correo enviado';
-            } else {
-                $_SESSION['mensaje'] = 'No se ha podido establecer la contraseña';
+                if (gestionDatos::setPassword($emailDestino, md5($az))) {
+                    $_SESSION['mensaje'] = 'Correo enviado';
+                } else {
+                    $_SESSION['mensaje'] = 'No se ha podido establecer la contraseña';
+                }
+                header('Location: ../Vistas/login.php');
+            } catch (Exception $e) {
+                $_SESSION['mensaje'] = 'No se ha podido enviar el correo';
             }
-            header('Location: ../Vistas/login.php');
-        } catch (Exception $e) {
-            $_SESSION['mensaje'] = 'No se ha podido enviar el correo';
+        } else {
+            $mensaje = 'Error captcha no superado.';
+            $_SESSION['mensaje'] = $mensaje;
+            header('Location: ../Vistas/olvidado.php');
         }
         ?>
+        
     </body>
 </html>
--- Mamas_2.0/Vistas/olvidado.php
+++ Mamas_2.0/Vistas/olvidado.php
@@ -14,8 +14,19 @@ and open the template in the editor.
         <link rel="stylesheet" href="../css/mdb.min.css">
         <!-- Your custom styles (optional) -->
         <link rel="stylesheet" href="../css/style.css">
+        <script src='https://www.google.com/recaptcha/api.js?render=6LdU7-QZAAAAANmiNBKJU677B_eGaE-tJsZL0TMT'>
+        </script>
+        <script>
+            grecaptcha.ready(function () {
+                grecaptcha.execute('6LdU7-QZAAAAANmiNBKJU677B_eGaE-tJsZL0TMT', {action: 'login'})
+                        .then(function (token) {
+                            var recaptchaResponse = document.getElementById('recaptchaResponse');
+                            recaptchaResponse.value = token;
+                        });
+            });
+        </script>
     </head>
-    <body>
+    <body onload="validacionLogin()">
         <div class="container my-5 px-0 z-depth-1">
             <!--Section: Content-->
             <section class="pb-5 my-md-5 text-center">
@@ -30,18 +41,29 @@ and open the template in the editor.
                                 <div class="card-body">
 
                                     <!-- Form -->
-                                    <form class="text-center needs-validation" style="color: #757575;" action="../Controlador/enviar.php" method="POST">
+                                    <form id="login" class="text-left needs-validation" style="color: #757575;" action="../Controlador/enviar.php" method="POST" novalidate>
                                         <h1 class="font-weight-bold my-4 pb-2 text-center tit">Forgot password?</h1>
-                                        <div class="md-form form-group ml-5 mr-5">
+                                        <div class="md-form form-group ml-5 mr-5 ">
                                             <input type="email" id="email" name="email" class="form-control mb-4 " required>
                                             <label class="form-control-label" for="email">E-mail</label>
+                                            <div name="emailError" class="" id="emailError"></div>
                                         </div>
+                                        <?php
+                                        session_start();
+                                        if (isset($_SESSION['mensaje'])) {
+                                            $mensaje = $_SESSION['mensaje'];
+                                            echo $mensaje . '<br>';
+                                            unset($_SESSION['mensaje']);
+                                        }
+                                        ?>
                                         <div class="text-center mb-3 pl-5 pr-5">
                                             <button type="submit" name="olvidado"  class="btn mean-fruit-gradient text-white btn-block btn-rounded my-4 waves-effect z-depth-1a">Send</button>
                                         </div>
                                         <div class="mx-5 pt-3 mb-1 text-right " >
                                             <a href="login.php" style="color: #D681E8"> >> Back</a>
                                         </div>
+
+                                        <input type="hidden" name="recaptcha_response" id="recaptchaResponse">
                                     </form>
                                 </div>
                             </div>
